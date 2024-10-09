945dfed3c238507a9889228afde902630d7e473c
Vinniel <155176879+Vinniel5403@users.noreply.github.com>
2024-10-09 10:11:23 +0700

    Add files via upload

---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+num = int(input("Enter a number\n"))
+i = 0
+while i <= 9:
+    print(f"{i} x {num} = {i*num}")
+    i = i+1
---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+i = 0
+while i <= 10:
+
+    print(f"{i*0} {i*1} {i*2} {i*3} {i*4} {i*5} {i*6} {i*7} {i*8} {i*9} {i*10}")
+
+    i = i+1
